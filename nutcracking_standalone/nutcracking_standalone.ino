2547c172453a3825c97e2164a8c07d304332ac33
Tino <tinodb@gmail.com>
2013-04-14 00:28:28 +0200

    sorta working. Runs on schizo

--- nutcracking_standalone/nutcracking_standalone.ino
+++ nutcracking_standalone/nutcracking_standalone.ino
@@ -1,94 +1,107 @@
-import processing.serial.*;
+// Constants and global vars
+// ----------
 
-Serial myPort;
+// Device ID
+#define ID 1
 
-int HZ = 20; // 50 millis resolution of arduino
+// Pins
+const int MAGNETIC_PIN = 7;
+const int TILT_PIN_1 = 7;
+const int TILT_PIN_2 = 8;
+
+// Thresholds
+const int HZ = 20; // 50 millis resolution of arduino
 float THRESHOLD = 3.0;
 float SHAKE_THRESHOLD_DOWN = 3.0;
 float SHAKE_THRESHOLD_UP = -1.5 ;
 float BALANCE_SHAKE_THRESHOLD = 3.0;
-String message;
-String inString;
-int state=0;
-String inMsg = "";
-int[] xQueue = new int[400];
-int [] yQueue = new int[400];
-int[] magneticQueue = new int[400];
-boolean _running = true;
+
+// Deque's (see bottom for push methods)
+#define HISTORY 100 // how many datapoints to rememeber
+int _xQueue[HISTORY];
+int _yQueue[HISTORY];
+int _magneticQueue[HISTORY];
+
+// Global variables
 int _orientation = 0;
 int _lastUpdateActivation = 0;
+int _lastHeartBeat = 0;
+int _lastLoop = 0;
+int _on = false;
 
-void setup() {
-  frameRate(HZ);
-  size(400, 600);
-  println(Serial.list());
-  String portName = Serial.list()[0];
-  myPort = new Serial(this, portName, 9600);
-  delay(1000);
-  myPort.clear();
-  delay(1000);
+void setup(){
+  // Start up our serial port, we configured our XBEE devices for 9600 bps.
+  Serial.begin(115200);
+  pinMode(TILT_PIN_1, INPUT);
+  pinMode(TILT_PIN_2, INPUT);
+  pinMode(13, OUTPUT);
+  int test[4];
+  int sum = 0;
+  for (int i=0; i<HISTORY; i++) {
+    sum += _magneticQueue[i];
+  }
+    Serial.println(sum);
 }
 
-float toYScreen(float y) {
-  return (y * 300) + 10;
-}
+void loop(){
+  if (millis() - _lastLoop > 1000/HZ)  {
+    _lastLoop = millis();
 
-void draw() {
-  if (_running) {
-    if (boolean(_orientation)) {
-      background(255);
-      fill(0);
-      stroke(0);
-    } else {
-      background(0);
-      fill(255);
-      stroke(255);
-    }
-    textSize(12);
-    text("0", 0, toYScreen(0));
-    text("1", 0, toYScreen(1));
-//    text(xQueue[xQueue.length-1], 0, 20);
-//    text(magneticQueue[magneticQueue.length-1], 0, 40);
-//    text(_lastUpdateActivation, 0, 60);
-    // line every second
-    for (int i=0; i<=400; i++) {
-      if (i % HZ == 0) {
-        line(i, toYScreen(0)-20, i, toYScreen(0)+20);
-      }
-    }
-    noFill();
-    stroke(196, 0, 0);
-    beginShape();
-    for (int i=0; i<xQueue.length; i++) {
-      curveVertex(i, toYScreen(xQueue[i]));
-    }
-    endShape();
-    stroke(104, 175, 244);
-    beginShape();
-    for (int i=0; i<yQueue.length; i++) {
-      curveVertex(i, toYScreen(yQueue[i]));
-    }
-    endShape();
+    int x = digitalRead(TILT_PIN_1);
+    int y = digitalRead(TILT_PIN_2);
+    int m = analogRead(MAGNETIC_PIN);
+    pushQueue(_xQueue, HISTORY, x);
+    pushQueue(_yQueue, HISTORY, y);
+    pushQueue(_magneticQueue, HISTORY, m);
+    // Serial.print(_xQueue[0]);
+    // Serial.print("\t");
+    // Serial.print(_yQueue[0]);
+    // Serial.print("\t");
+    // Serial.println(_magneticQueue[0]);
+    setOrientation();
+    setUpdateActivation();
+    // sendData("orientation", _orientation);
+    sendData("lastupdate", _lastUpdateActivation);
 
     if (updateActivated()) {
-      fill(255, 255, 255);
-      rect(250, 10, 40, 40);
       if (checkForUpdate()) {
-        fill(196, 0, 0);
-        rect(350, 10, 40, 40);
+        // TODO: make sure this happens once per updateActivated cycle
+        sendData("update", 1);
       }
-    } else {
+    }
+    else {
       if (checkForBalanceCheck()) {
-        fill(0, 200, 0);
-        rect(350, 50, 40, 40);
+        sendData("balance", 1);
       }
+    }
+  }
+  if (millis() - _lastHeartBeat > 1000) {
+    sendData("Alive", 1);
+    _lastHeartBeat = millis();
+    if (_on) {
+      digitalWrite(13, 0);
+      _on = false;
+    } else {
+      digitalWrite(13, 1);
+      _on = true;
     }
   }
 }
 
+void sendData(char key[], int value) {
+  Serial.print("<<");
+  Serial.print(ID);
+  Serial.print(":");
+  Serial.print(key);
+  Serial.print(":");
+  Serial.print(value);
+  Serial.println(">>");
+}
+
+
 boolean updateActivated() {
   if (_lastUpdateActivation > 0)
-    return millis() - _lastUpdateActivation < 3 * 1000;
+    return (millis() - _lastUpdateActivation) < 3 * 1000;
   return false;
 }
 
@@ -97,9 +110,10 @@ void setUpdateActivation() {
   // sensor to be activated
   int items = HZ / 4;
   int sum = 0;
-  for(int i=magneticQueue.length-1; i > (magneticQueue.length - items - 1); i--) {
-    sum = sum + magneticQueue[i];
+  for(int i=0; i < items; i++) {
+    sum += _magneticQueue[i];
   }
+  Serial.println(sum / items);
   if (sum / items > 950) {
     _lastUpdateActivation = millis();
   }
@@ -111,12 +125,11 @@ void setOrientation() {
   // 1 / 1 is palm down, sorta... :)
   // average over the half second
   int sum = 0;
-  for (int i=1; i < HZ/2 + 1; i++) {
-    if (xQueue[xQueue.length - i] == yQueue[yQueue.length - i]) {
-      sum = sum + xQueue[xQueue.length - i];
+  for (int i=1; i < HZ/2 + 1 ; i++) {
+    if (_xQueue[i] == _yQueue[i]) {
+      sum = sum + _xQueue[i];
     }
   }
-
   if ((sum / float(HZ/2)) > 0.8) {
     _orientation = 1;
   } else if ((sum / float(HZ/2)) < 0.1) {
@@ -128,11 +141,11 @@ boolean checkForBalanceCheck() {
   // Check for continuous peaks in the last 2 seconds. A positive, negative and positive peak
   // mean a shake. Peaks are counted as sign changes.
   int items = 2 * HZ;
-  float[] diffs = new float[items];
+  float diffs[items];
 
   int changes = 0;
   for (int i=1; i<items; i++) {
-    if (xQueue[xQueue.length-i] != xQueue[xQueue.length-i-1])
+    if (_xQueue[HISTORY-i] != _xQueue[HISTORY-i-1])
       changes = changes + 1;
   }
 //  print("changes: ");
@@ -149,15 +162,13 @@ boolean checkForBalanceCheck() {
 boolean checkForUpdate() {
   // Check for one single peak in the last second
   int items = HZ;
-  int[] diffs = new int[items];
+  int diffs[items];
 
   int changes = 0;
   for (int i=1; i<items; i++) {
-    if (xQueue[xQueue.length-i] != xQueue[xQueue.length-i-1])
+    if (_xQueue[HISTORY-i] != _xQueue[HISTORY-i-1])
       changes = changes + 1;
   }
-
-  println(changes);
 
   if (changes == 2) {
     return true;
@@ -166,56 +177,26 @@ boolean checkForUpdate() {
   }
 }
 
-
-void serialEvent(Serial p) {
-  try {
-    inString = p.readStringUntil('\n');
-    if (inString!=null) {
-      inString = trim(inString);
-      String[] values = splitTokens(inString);
-      if (values.length == 3) {
-        try {
-          int x = int(values[0]);
-          int y = int(values[1]);
-          int m = int(values[2]);
-//          int m = int(values[3]);
-          pushXQueue(x);
-          pushYQueue(y);
-          pushMagneticQueue(m);
-          setOrientation();
-          setUpdateActivation();
-        } catch (Exception e) {
-          println("Error converting values");
-        }
-
-      }
-    }
-  } catch(Exception e) {
-    println("Error reading serial");
+// Helper functions to create deque's
+void pushQueue(int array[], int size, int v) {
+  for (int i = 0; i < size-1; i++) {
+    array[i+1] = array[i];
   }
-}
+  array[0] = v;
 
-void keyPressed() {
-  if (_running) {
-    _running = false;
-  } else {
-    _running = true;
-  }
 }
-
-
-void pushXQueue(int v) {
-  int[] newQueue = subset(xQueue, 1);
-  int[] newQueue2 = append(newQueue, v);
-  xQueue = newQueue2;
-}
-void pushYQueue(int v) {
-  int[] newQueue = subset(yQueue, 1);
-  int[] newQueue2 = append(newQueue, v);
-  yQueue = newQueue2;
-}
-void pushMagneticQueue(int v) {
-  int[] newQueue = subset(magneticQueue, 1);
-  int[] newQueue2 = append(newQueue, v);
-  magneticQueue = newQueue2;
-}
+// void pushXQueue(int v) {
+//   int newQueue[HISTORY-1] = subset(_xQueue, 1);
+//   int newQueue2[HISTORY] = append(newQueue, v);
+//   _xQueue = newQueue2;
+// }
+// void pushYQueue(int v) {
+//   int newQueue[HISTORY-1] = subset(_yQueue, 1);
+//   int newQueue2[HISTORY] = append(newQueue, v);
+//   _yQueue = newQueue2;
+// }
+// void pushMQueue(int v) {
+//   int newQueue[HISTORY-1] = subset(_magneticQueue, 1);
+//   int newQueue2[HISTORY] = append(newQueue, v);
+//   _magneticQueue = newQueue2;
+// }
