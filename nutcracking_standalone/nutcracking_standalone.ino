2e426386a4d00d33afaee414817b3dc257e5f649
Tino <tinodb@gmail.com>
2013-04-17 22:11:31 +0200

    Code cleanup and reordering
    DEBUG is now a level
    Removed old afro stuff
    Better constants
    Better naming of constants related to update window

--- nutcracking_standalone/nutcracking_standalone.ino
+++ nutcracking_standalone/nutcracking_standalone.ino
@@ -8,32 +8,34 @@
 
 //enable to show debugging information about parsing and operations
 //not so good on broadcast channels
-const boolean DEBUG = false;
+const int DEBUG = 0;
 
 // Pins
 const int MAGNETIC_PIN = 7;
 const int TILT_PIN_1 = 7;
 const int TILT_PIN_2 = 8;
 
-// Thresholds
+// Times
 const int HZ = 10;
-const int UPDATE_ACTIVATION_WINDOW = 1 * 1000; // 10 seconds
-const int UPDATE_WINDOW = 10 * 1000; // 10 seconds
-float THRESHOLD = 3.0;
-float SHAKE_THRESHOLD_DOWN = 3.0;
-float SHAKE_THRESHOLD_UP = -1.5 ;
-float BALANCE_SHAKE_THRESHOLD = 3.0;
+const int UPDATE_ACTIVATION_DURATION = 1 * 1000;
+const int UPDATE_WINDOW_DURATION = 10 * 1000;
+
+// Thresholds
+const float THRESHOLD = 3.0;
+const float SHAKE_THRESHOLD_DOWN = 3.0;
+const float SHAKE_THRESHOLD_UP = -1.5 ;
+const float BALANCE_SHAKE_THRESHOLD = 3.0;
 
 // Messaging commands
-#define UPDATE_SYN        'A' // Start update sync
-#define UPDATE_ACK        'B' // Acknowledge update sync
-#define UPDATE_OPEN       'C' // Update window is open
+#define UPDATE_WINDOW_SYN        'A' // Start update sync
+#define UPDATE_WINDOW_ACK        'B' // Acknowledge update sync
+#define UPDATE_WINDOW_OPEN       'C' // Update window is open
 
 // States
-#define PASSIVE           1
-#define UPDATE_ACTIVATED  2
-#define UPDATE_ACKED      3
-#define UPDATE_OPENED     4
+#define PASSIVE                  1
+#define UPDATE_WINDOW_ACTIVATED  2
+#define UPDATE_WINDOW_ACKED      3
+#define UPDATE_WINDOW_OPENED     4
 
 
 // Deque's (see bottom for push methods)
@@ -49,32 +51,19 @@ unsigned long _lastUpdateActivation = 0;
 unsigned long _updateWindowOpenStart = 0;
 
 
-// Afro stuff
-#define REGISTERSIZE 10
-long int reg[REGISTERSIZE];
-
-//Afro will delay this much before answering a broadcast message, to make collisions less likely
-int broadcastDelay = (ID*79)%255;
-
-unsigned char res0='Z'; //the secondary result from the last query
-int res=0;              //the result from the last query
-
-
 void setup(){
-  // Start up our serial port, we configured our XBEE devices for 9600 bps.
+  // Start up our serial port, we configured our XBEE devices for 57600 bps.
   Serial.begin(57600);
-  // Initiate Firmata
-  // Firmata.setFirmwareVersion(1, 1);
-  // Firmata.attach(START_SYSEX, sysexCallback);
-  // Firmata.begin(115200);
 
   // Setup pins
   pinMode(TILT_PIN_1, INPUT);
   pinMode(TILT_PIN_2, INPUT);
   pinMode(13, OUTPUT);
 
-  Serial.print(ID);
-  Serial.println(" has entered the building");
+  if (DEBUG) {
+    Serial.print(ID);
+    Serial.println(" has entered the building");
+  }
 
 }
 
@@ -88,6 +77,7 @@ void loop(){
   if (millis() - _lastLoop > 1000/HZ)  {
     _lastLoop = millis();
 
+    // Update our readings
     int x = digitalRead(TILT_PIN_1);
     int y = digitalRead(TILT_PIN_2);
     int m = analogRead(MAGNETIC_PIN);
@@ -95,17 +85,19 @@ void loop(){
     pushQueue(_yQueue, HISTORY, y);
     pushQueue(_magneticQueue, HISTORY, m);
 
+    // Process readings
     setOrientation();
     setUpdateActivation();
 
+    // Act according to state
     switch (_state) {
 
       case PASSIVE:
         if (updateActivated()) {
           // We are passive and update was activated, so broadcast
           sendDebug("updateActivated", 1);
-          _state = UPDATE_ACTIVATED;
-          sendRequest('0', UPDATE_SYN, 'x', 'x');
+          _state = UPDATE_WINDOW_ACTIVATED;
+          sendRequest('0', UPDATE_WINDOW_SYN, 'x', 'x');
         }
         if (checkForBalanceCheck()) {
           sendDebug("balance", 1);
@@ -113,21 +105,29 @@ void loop(){
         }
       break;
 
-      case UPDATE_ACTIVATED:
+      case UPDATE_WINDOW_ACTIVATED:
         // if updateActivation is over reset state
         if (!updateActivated()) _state = PASSIVE;
       break;
 
-      case UPDATE_ACKED:
+      case UPDATE_WINDOW_ACKED:
         // if updateActivation is over reset state and we havent' moved to
-        // UPDATE_OPEN in the mean time, reset
+        // UPDATE_WINDOW_OPEN in the mean time, reset
         if (!updateActivated()) _state = PASSIVE;
         // TODO: close window, so the other side is closed for sure as well?
       break;
 
-      case UPDATE_OPENED:
+      case UPDATE_WINDOW_OPENED:
         // If the time window has closed, reset state to PASSIVE
-        if (millis() - _updateWindowOpenStart > UPDATE_WINDOW) _state = PASSIVE;
+        if (millis() - _updateWindowOpenStart > UPDATE_WINDOW_DURATION) {
+          _state = PASSIVE;
+          break;
+        }
+
+        // Check for an update shake
+        if (checkForUpdate()) {
+          // send message to window partner
+        }
       break;
     }
   }
@@ -149,27 +149,9 @@ void loop(){
 }
 
 
-
-void processSerial() {
-  if (Serial.available()>0){ //characters available for processing
-    processBuffer();
-  }
-}
-
-void sendDebug(char key[], int value) {
-  Serial.print("<");
-  Serial.print(ID);
-  Serial.print(":");
-  Serial.print(key);
-  Serial.print(":");
-  Serial.print(value);
-  Serial.println(">");
-}
-
-
 boolean updateActivated() {
   if (_lastUpdateActivation > 0)
-    return (millis() - _lastUpdateActivation) < UPDATE_ACTIVATION_WINDOW;
+    return (millis() - _lastUpdateActivation) < UPDATE_WINDOW_DURATION;
   return false;
 }
 
@@ -274,16 +256,65 @@ void pushQueue(int array[], int size, int v) {
 // 6: primary res LSB
 // 78: RS
 
-// void sendResponse(byte to, byte res, byte res2) {
-//   // Reply to a message. to is the ID of the targeted machine
-//   Serial.print("SR");
-//   Serial.write(to);
-//   Serial.write(ID);
-//   Serial.write(res2);           //secondary result, one byte
-//   Serial.write(highByte(res));  //primary result, two bytes
-//   Serial.write(lowByte(res));
-//   Serial.print("RS");
-// }
+
+void execute(unsigned char from, unsigned char operation, unsigned char operand1, int operand2){
+  if (DEBUG){
+    Serial.println("----");
+    Serial.print(" from=");
+    Serial.print(from);
+    Serial.print(" operation=");
+    Serial.print(operation);
+    Serial.print(" operand1=");
+    Serial.print(operand1);
+    Serial.print(" operand2=");
+    Serial.println(operand2);
+    Serial.println("----");
+  }
+
+  switch (operation) {
+    case UPDATE_WINDOW_SYN:
+      if (DEBUG) {
+        Serial.print("UPDATE_WINDOW_SYN; state: ");
+        Serial.println(_state);
+      }
+      // If we are in UPDATE_WINDOW_ACTIVATED state as well, we respond
+      if (_state == UPDATE_WINDOW_ACTIVATED) {
+        // We are ready to do an update, let's see who it is and reply
+        sendRequest(from, UPDATE_WINDOW_ACK, '0', '0');
+        _state = UPDATE_WINDOW_ACKED;
+      }
+      // We are either passive, or already in conversation with someone, so
+      // ignore
+    break;
+
+    case UPDATE_WINDOW_ACK:
+      // Someone responds to an UPDATE_WINDOW_SYN request. Are we still
+      // in the UPDATE_WINDOW_ACTIVATED state?
+      if (DEBUG) {
+        Serial.print("UPDATE_WINDOW_ACK; state: ");
+        Serial.println(_state);
+      }
+
+      if (_state == UPDATE_WINDOW_ACKED || _state == UPDATE_WINDOW_ACTIVATED) {
+        // Oke, activation window is open
+        sendRequest(from, UPDATE_WINDOW_OPEN, '0', '0');
+        _state = UPDATE_WINDOW_OPENED;
+        _updateWindowOpenStart = millis();
+      }
+    break;
+
+    case UPDATE_WINDOW_OPEN:
+      // Response to UPDATE_WINDOW_ACK, so we should be in UPDATE_WINDOW_ACKED state
+      if (DEBUG) {
+        Serial.print("UPDATE_WINDOW_OPEN; state: ");
+        Serial.println(_state);
+      }
+      if (_state != UPDATE_WINDOW_ACKED) return;
+      // Window is opened on the other side, open here as well
+      _state = UPDATE_WINDOW_OPENED;
+      _updateWindowOpenStart = millis();
+  }
+}
 
 void sendRequest(unsigned char to, unsigned char operation, unsigned char operand1, unsigned char operand2) {
   // Send out a message. to is the ID of the targeted machine
@@ -299,22 +330,36 @@ void sendRequest(unsigned char to, unsigned char operation, unsigned char operan
   Serial.print("FA");
 }
 
-void processBuffer() {
+// void sendResponse(byte to, byte res, byte res2) {
+//   // Reply to a message. to is the ID of the targeted machine
+//   Serial.print("SR");
+//   Serial.write(to);
+//   Serial.write(ID);
+//   Serial.write(res2);           //secondary result, one byte
+//   Serial.write(highByte(res));  //primary result, two bytes
+//   Serial.write(lowByte(res));
+//   Serial.print("RS");
+// }
+
+void processSerial() {
+  if (!Serial.available()) return;
+
   static short int state = 0;
   static unsigned char from = 255;
   static unsigned char operation = 255;
   static unsigned char operand1 = 255;
   static int operand2 = -1;
-  char c;
-  c= Serial.read();
-  // if (DEBUG){
-  //   digitalWrite(13,HIGH);
-  //   delay(5);
-  //   digitalWrite(13,LOW);
-  //   Serial.print(state);
-  //   Serial.print(" ");
-  //   Serial.println(c);
-  // }
+
+  char c = Serial.read();
+
+  if (DEBUG > 1){
+    digitalWrite(13,HIGH);
+    delay(5);
+    digitalWrite(13,LOW);
+    Serial.print(state);
+    Serial.print(" ");
+    Serial.println(c);
+  }
 
   switch(state) {
 
@@ -396,64 +441,15 @@ void processBuffer() {
   }
 }
 
-void execute(unsigned char from, unsigned char operation, unsigned char operand1, int operand2){
-  if (DEBUG){
-    Serial.println("----");
-    Serial.print(" from=");
-    Serial.print(from);
-    Serial.print(" operation=");
-    Serial.print(operation);
-    Serial.print(" operand1=");
-    Serial.print(operand1);
-    Serial.print(" operand2=");
-    Serial.println(operand2);
-    Serial.println("----");
-  }
 
-  switch (operation) {
-    case UPDATE_SYN:
-      if (DEBUG) {
-        Serial.print("UPDATE_SYN; state: ");
-        Serial.println(_state);
-      }
-      // If we are in UPDATE_ACTIVATED state as well, we respond
-      if (_state == UPDATE_ACTIVATED) {
-        // We are ready to do an update, let's see who it is and reply
-        sendRequest(from, UPDATE_ACK, '0', '0');
-        _state = UPDATE_ACKED;
-      }
-      // We are either passive, or already in conversation with someone, so
-      // ignore
-    break;
-
-    case UPDATE_ACK:
-      // Someone responds to an UPDATE_SYN request. Are we still
-      // in the UPDATE_ACTIVATED state?
-      if (DEBUG) {
-        Serial.print("UPDATE_ACK; state: ");
-        Serial.println(_state);
-      }
-
-      if (_state == UPDATE_ACKED || _state == UPDATE_ACTIVATED) {
-        // Oke, activation window is open
-        sendRequest(from, UPDATE_OPEN, '0', '0');
-        _state = UPDATE_OPENED;
-        _updateWindowOpenStart = millis();
-      }
-    break;
-
-    case UPDATE_OPEN:
-      // Response to UPDATE_ACK, so we should be in UPDATE_ACKED state
-      if (DEBUG) {
-        Serial.print("UPDATE_OPEN; state: ");
-        Serial.println(_state);
-      }
-      if (_state != UPDATE_ACKED) return;
-      // Window is opened on the other side, open here as well
-      _state = UPDATE_OPENED;
-      _updateWindowOpenStart = millis();
-  }
+void sendDebug(char key[], int value) {
+  if (DEBUG == 0) return;
+  Serial.print("<");
+  Serial.print(ID);
+  Serial.print(":");
+  Serial.print(key);
+  Serial.print(":");
+  Serial.print(value);
+  Serial.println(">");
 }
-
-
 
